e8863c5a4f8951842d1086ae4ef52dac6df89e6f
Will <yh@fir.im>
2017-02-15 16:52:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# mongodb_database Step
+Launch MongoDB database
+
+### INPUTS
+
+## EXAMPLE 
+
+```yml
+steps:
+  - name: mongodb_database
+    enable: true
+    failure: true
+    plugin:
+      name: mongodb_database
+      inputs:
+```
---
+++ run.sh
@@ -0,0 +1,17 @@
+# ************************************************************
+#
+# This step will start mongodb
+#
+#   Variables used:
+#
+#   Outputs:
+#     $FLOW_DATABASE_MONGODB_VERSION
+#     $FLOW_DATABASE_MONGODB_IP
+#     $FLOW_DATABASE_MONGODB_PORT
+#
+# ************************************************************
+
+flow_cmd "sudo /etc/init.d/mongodb start" --echo
+FLOW_DATABASE_MONGODB_VERSION="$(mongo --version 2>&1)"
+FLOW_DATABASE_MONGODB_PORT="27017"
+FLOW_DATABASE_MONGODB_IP="127.0.0.1"
